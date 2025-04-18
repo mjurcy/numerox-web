1287b35b3c28ce47bdaec6873af98f951dc685e7
mjurcy <65263265+mjurcy@users.noreply.github.com>
2025-04-17 23:50:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-document.getElementById("licenseForm").addEventListener("submit", async (e) => {
-  e.preventDefault();
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("licenseForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-  const email = document.getElementById("email").value.trim();
-  const key = document.getElementById("key").value.trim();
-  const result = document.getElementById("result");
+    const email = document.getElementById("email").value.trim();
+    const key = document.getElementById("key").value.trim();
+    const result = document.getElementById("result");
 
-  result.innerHTML = "Validando...";
-  result.style.color = "#000";
+    result.innerHTML = "Validando...";
+    result.style.color = "#000";
 
-  try {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbwx1-n_rv5Vdf3TjI3WBKkbC_o8SW2b4iBrvfczGWrjn1umgtqr3h3FU46qG3UNgWSIBQ/exec', {
-      method: 'POST',
-      body: new URLSearchParams({ email, key })
-    });
+    try {
+      const response = await fetch('https://script.google.com/macros/s/AKfycbzWc136X6RQ3dc-LspqBJiP6upHWkV2DXMrE9OiDg03__oX1Lzx-zRFuSMXe7TtI9WVkg/exec', {
+        method: 'POST',
+        body: new URLSearchParams({ email, key })
+      });
 
-    const data = await response.json();
+      const data = await response.json();
 
-    if (data.ok) {
-      result.innerHTML = `✔ ${data.message}`;
-      result.style.color = "green";
-    } else {
-      result.innerHTML = `✗ ${data.message}`;
+      if (data.ok) {
+        result.innerHTML = `✔ ${data.message}`;
+        result.style.color = "green";
+      } else {
+        result.innerHTML = `✗ ${data.message}`;
+        result.style.color = "red";
+      }
+
+    } catch (error) {
+      result.innerHTML = `✗ Error del servidor: ${error}`;
       result.style.color = "red";
     }
-
-  } catch (error) {
-    result.innerHTML = `✗ Error del servidor: ${error}`;
-    result.style.color = "red";
-  }
+  });
 });
